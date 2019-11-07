5109576086a4bf508232ec77683578905671db15
alonso1311 <alonso.barrios@utec.edu.pe>
2019-11-07 03:59:05 -0500

    6 Ejercicios

---
+++ Sort/DNA_Sorting.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+
+using namespace std;
+
+int inversion(string s1) {
+    int count_inversion = 0;
+    for(size_t i = 0; i < s1.size(); i++)
+        for(size_t j = i; j < s1.size()-i-1; j++)
+            if(s1[i] > s1[j])
+                count_inversion++;
+
+    return count_inversion;
+}
+
+void DNA_Sorting() {
+    size_t number_cases;
+    cin >> number_cases;
+    cin.ignore();
+
+    cout << endl;
+
+    for(; number_cases > 0; number_cases--) {
+        size_t size_DNA, number_DNA;
+        cin >> size_DNA >> number_DNA;
+
+        vector<string> v_DNA;
+        for(; number_DNA > 0; number_DNA--) {
+            char DNA [size_DNA];
+            cin >> DNA;
+            cin.ignore();
+
+            v_DNA.push_back(DNA);
+        }
+
+        cout << endl;
+
+        stable_sort(begin(v_DNA), end(v_DNA), [](string s1, string s2){return inversion(s1) < inversion(s2);});
+        
+        for(const auto& i : v_DNA)
+            cout << i << endl;
+    }
+
+}
+
+int main() {
+    DNA_Sorting(); 
+    return 0;
+}
